db20304d156489e1a8cf95a9ca67891b4905b601
Criss <83183708+TienMinh1998@users.noreply.github.com>
2022-11-23 18:32:19 +0700

    Create UserController.cs

---
+++ Hola.Api/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Hola.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Hola.Api.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+                
+        }
+
+
+
+        [HttpPost("Login")]
+        public async Task<JsonResponseModel> Search()
+        {
+            return JsonResponseModel.Success();
+        }
+    }
+}
