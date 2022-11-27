6765b26aa1095e764ee97f66b2511bdac4f4189b
Criss <83183708+TienMinh1998@users.noreply.github.com>
2022-11-28 00:51:43 +0700

    Update

--- DatabaseCore.Domain/Entities/Normals/User.cs
+++ DatabaseCore.Domain/Entities/Normals/User.cs
@@ -13,6 +13,7 @@ namespace DatabaseCore.Domain.Entities.Normals
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
-    
+
+        public string Name { get; set; }
     }
 }
