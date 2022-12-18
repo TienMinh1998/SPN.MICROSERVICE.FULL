debf1b956701341e1aac77bbde77b8c42b268170
Criss <83183708+TienMinh1998@users.noreply.github.com>
2022-12-18 20:51:48 +0700

    123

--- Hola.Api/Service/Quatz/JobScheduler.cs
+++ Hola.Api/Service/Quatz/JobScheduler.cs
@@ -26,7 +26,7 @@ namespace Hola.Api.Service.Quatz
             quartz.AddJob<T>(opts => opts.WithIdentity(jobKey));
             quartz.AddTrigger(opts => opts
                 .ForJob(jobKey)
-                .WithIdentity(jobName + "-trigger").WithCronSchedule("* /3 * * * ?"));
+                .WithIdentity(jobName + "-trigger").WithCronSchedule("* * /3 * * ?"));
                // .WithSimpleSchedule(s => s.WithIntervalInMinutes(10).RepeatForever()));
         }
 
