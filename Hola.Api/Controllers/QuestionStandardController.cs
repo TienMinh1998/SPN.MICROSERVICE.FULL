7152f54baaca2aac75a55d17268d68ac8e095043
Criss <83183708+TienMinh1998@users.noreply.github.com>
2023-01-07 00:41:32 +0700

    Update QuestionStandardController.cs

--- Hola.Api/Controllers/QuestionStandardController.cs
+++ Hola.Api/Controllers/QuestionStandardController.cs
@@ -139,9 +139,10 @@ namespace Hola.Api.Controllers
         {
             try
             {
-                string query = "SELECT  a.\"English\", a.\"Phonetic\" , a.\"MeaningEnglish\",  a.\"MeaningVietNam\"   FROM  (public.\"QuestionStandards\" q " +
+                string query = "SELECT a.\"Pk_QuestionStandard_Id\",  a.\"English\", a.\"Phonetic\" , a.\"MeaningEnglish\",  a.\"MeaningVietNam\"   FROM  (public.\"QuestionStandards\" q " +
                     "\r\ninner join usr.\"QuestionStandardDetail\" qd on q.\"Pk_QuestionStandard_Id\"" +
                     $" = qd.\"QuestionID\" ) a\r\ninner join usr.topic tq on tq.\"PK_Topic_Id\" = a.\"TopicID\"\r\nwhere a.\"TopicID\" = {request.TargetID}";
+
                 var response = await _dapper.GetAllAsync<QuestionStandardModel>(query.AddPadding(request.pageNumber, request.PageSize));
                 return JsonResponseModel.Success(response);
                 
@@ -255,7 +256,7 @@ namespace Hola.Api.Controllers
 
         [HttpPost("Tick")]
         [Authorize]
-        public async Task<JsonResponseModel> TickQuestion(TickQuestionRequest request)
+        public async Task<JsonResponseModel> TickQuestion([FromBody] TickQuestionRequest request)
         {
             try
             {
