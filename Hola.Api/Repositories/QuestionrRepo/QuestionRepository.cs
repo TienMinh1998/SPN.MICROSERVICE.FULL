b9ea03be2bf2152ef6a3a0b4d7b727708382cc47
TienMinh <83183708+TienMinh1998@users.noreply.github.com>
2023-01-16 08:33:03 +0700

    Update QuestionRepository.cs

--- Hola.Api/Repositories/QuestionrRepo/QuestionRepository.cs
+++ Hola.Api/Repositories/QuestionrRepo/QuestionRepository.cs
@@ -17,7 +17,7 @@ namespace Hola.Api.Repositories.QuestionrRepo
             return await Task.Run(() =>
              {
                  var model = (from qe in DbContext.Questions
-                              where qe.fk_userid == userid && qe.created_on.Day == System.DateTime.Now.Day
+                              where qe.fk_userid == userid && qe.created_on.AddDays(7).Day == System.DateTime.Now.Day
                               select qe.fk_userid).Count();
                  return model;
              });
