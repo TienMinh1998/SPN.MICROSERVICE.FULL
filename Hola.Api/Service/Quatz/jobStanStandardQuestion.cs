5ed17e3457753e736469e60857480d86deb44b89
TienMinh <83183708+TienMinh1998@users.noreply.github.com>
2023-03-24 16:51:15 +0700

    Update jobStanStandardQuestion.cs

--- Hola.Api/Service/Quatz/jobStanStandardQuestion.cs
+++ Hola.Api/Service/Quatz/jobStanStandardQuestion.cs
@@ -45,7 +45,7 @@ namespace Hola.Api.Service.Quatz
                         notification = new NotificationMessageBody()
                         {
                             title = $"{question.English}",
-                            body = $"{question.Phonetic}"
+                            body = $"{question.Note}"
                         }
                     };
                     request.registration_ids.Add(devideFirebaseToken);
