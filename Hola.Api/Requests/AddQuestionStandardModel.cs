525d783c69e6a7b3d824016155f83f7d28723054
TienMinh <83183708+TienMinh1998@users.noreply.github.com>
2023-04-28 13:23:04 +0700

    To do

--- Hola.Api/Requests/AddQuestionStandardModel.cs
+++ Hola.Api/Requests/AddQuestionStandardModel.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 
-namespace Hola.Api.Requests
+namespace Hola.Api.Requests;
+
+public class AddQuestionStandardModel
 {
-    public class AddQuestionStandardModel
-    {
-        public string English { get; set; }          // Từ bằng tiếng anh
-        public string Phonetic { get; set; }         // Phiên tâm
-        public string MeaningEnglish { get; set; }   // Nghĩa tiếng anh
-        public string MeaningVietNam { get; set; }   // Nghĩa tiếng việt
-        public string Type { get; set; }             // Kiểu từ
-        public string Note { get; set; }             // Ghi chú
-    }
+    public string English { get; set; }          // Từ bằng tiếng anh
+    public string Phonetic { get; set; }         // Phiên tâm
+    public string MeaningEnglish { get; set; }   // Nghĩa tiếng anh
+    public string MeaningVietNam { get; set; }   // Nghĩa tiếng việt
+    public string Type { get; set; }             // Kiểu từ
+    public string Note { get; set; }             // Ghi chú
 }
